0b30878eb150d8e1fe70b76a1876afe99eed740b
Tanner Kirkpatrick <twk4491@gmail.com>
2021-02-26 20:52:51 -0500

    navbar component completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <img src="/img/logo-endless.svg" alt="endless-logo" className="logo" />
+    </nav>
+  );
+};
+
+export default Navbar;
