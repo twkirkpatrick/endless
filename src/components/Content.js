5dce9ef3a86ac68541425877084a323fa9dd7ef1
Tanner Kirkpatrick <twk4491@gmail.com>
2021-02-27 11:41:41 -0500

    main content box built out, have to adjust for responsiveness

---
+++ src/components/Content.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div className="content">
+      <h4>New Games & Accessories</h4>
+      <h1>
+        <span>Monthly Packages.</span>
+        <span>Excitement delivered daily.</span>
+      </h1>
+      <div style={{ width: "40%", lineHeight: "1.7" }}>
+        <p>
+          What's the best way to shop for the latest video games and
+          peripherals? How about never shopping at all? You'll get new stuff on
+          your doorstep - every month.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import background from "../img/photo-couch.jpg";
+import Content from "./Content";
 
 const Main = () => {
   return (
@@ -8,9 +9,12 @@ const Main = () => {
         background: `url(${background}) no-repeat center center fixed`,
         backgroundSize: "cover",
         width: "100%",
-        height: "100vh"
+        height: "100vh",
+        position: "relative"
       }}
-    ></main>
+    >
+      <Content />
+    </main>
   );
 };
 
